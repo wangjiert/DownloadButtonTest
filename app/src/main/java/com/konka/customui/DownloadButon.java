f55b377c1e8ed86ad3797abf8159d6e853412362
wangjiert <wangjiert@gmail.com>
2016-09-07 18:03:29 +0800

    增加动画效果

--- app/src/main/java/com/konka/customui/DownloadButon.java
+++ app/src/main/java/com/konka/customui/DownloadButon.java
@@ -1,5 +1,9 @@
 package com.konka.customui;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
@@ -11,6 +15,7 @@ import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.BounceInterpolator;
 
 import com.konka.downloadbuttontest.R;
 
@@ -18,13 +23,21 @@ import com.konka.downloadbuttontest.R;
  * TODO: document your custom view class.
  */
 public class DownloadButon extends View {
+    private int drawableBlank = 40;
     private Drawable startDrawable;
+    private Drawable completeDrawable;
+    private Drawable drawDrawable;
     private Paint paint;
     private float paintWidth = 40;                  //painstrokewidth的初始值
     private final float startAngle = -90;
     private float totalPosition;
     private float currentPosition;
     private int paintColor = Color.BLUE;                   //paint的初始颜色
+    private int drawableTop;
+    private ValueAnimator drawableUpAnimator;
+    private ValueAnimator drawableDownAnimator;
+    private ValueAnimator completeAnimator;
+    private AnimatorSet animatorSet = new AnimatorSet();
     public DownloadButon(Context context) {
         super(context);
         init(null, 0);
@@ -41,7 +54,13 @@ public class DownloadButon extends View {
     }
 
     private void init(AttributeSet attrs, int defStyle) {
-        // Load attributes
+        setClickable(true);
+        setFocusable(true);
+        int width = View.MeasureSpec.makeMeasureSpec( 0, MeasureSpec.UNSPECIFIED);
+        int height = View.MeasureSpec.makeMeasureSpec( 0, MeasureSpec.UNSPECIFIED);
+        measure( width, height);
+        width = getMeasuredWidth();
+        height = getMeasuredHeight();
         final TypedArray a = getContext().obtainStyledAttributes(
                 attrs, R.styleable.DownloadButon, defStyle, 0);
 
@@ -51,6 +70,12 @@ public class DownloadButon extends View {
             startDrawable = a.getDrawable(
                     R.styleable.DownloadButon_startDrawable);
             startDrawable.setCallback(this);
+            drawDrawable = startDrawable;
+        }
+        if (a.hasValue(R.styleable.DownloadButon_completeDrawable)) {
+            completeDrawable = a.getDrawable(
+                    R.styleable.DownloadButon_completeDrawable);
+            completeDrawable.setCallback(this);
         }
         a.recycle();
         paint = new Paint();
@@ -59,14 +84,64 @@ public class DownloadButon extends View {
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth(paintWidth);
         paint.setColor(paintColor);
+        drawableTop = (int) (height - drawDrawable.getIntrinsicHeight()) / 2;
+        ValueAnimator.AnimatorUpdateListener animatorUpdateListener = new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                setDrawableTop((int)valueAnimator.getAnimatedValue());
+            }
+        };
+        drawableUpAnimator = ValueAnimator.ofInt(drawableTop, (int) (getPaddingTop() + paintWidth - drawableBlank));
+        drawableUpAnimator.setDuration(200);
+        drawableUpAnimator.addUpdateListener(animatorUpdateListener);
+        drawableUpAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
+                drawDrawable = startDrawable;
+            }
+        });
+        drawableDownAnimator = ValueAnimator.ofInt((int) (getPaddingTop() + paintWidth - drawableBlank), (int) (height - getPaddingBottom() - paintWidth - drawDrawable.getIntrinsicHeight() + drawableBlank));
+        drawableDownAnimator.setDuration(400);
+        drawableDownAnimator.addUpdateListener(animatorUpdateListener);
+        drawableDownAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                drawDrawable = completeDrawable;
+            }
+        });
+        completeAnimator = ValueAnimator.ofInt(0, (int) (height - drawDrawable.getIntrinsicHeight()) / 2);
+        completeAnimator.setDuration(600);
+        completeAnimator.addUpdateListener(animatorUpdateListener);
+        completeAnimator.setInterpolator(new BounceInterpolator());
+        animatorSet.play(drawableUpAnimator).before(drawableDownAnimator);
+        animatorSet.play(drawableDownAnimator).before(completeAnimator);
     }
 
-    public void setTotalPosition(float totalPosition) {
+    public void setTotalPosition (float totalPosition) {
         this.totalPosition = totalPosition;
     }
 
-    public void setCurrentPosition(float currentPosition) {
+    public void setCurrentPosition (float currentPosition) {
         this.currentPosition = currentPosition;
+        if(currentPosition == totalPosition){
+            animatorSet.start();
+        }
+    }
+
+    private void setDrawableTop (int top){
+        this.drawableTop = top;
+        invalidate();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec){
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        setMeasuredDimension(300,300);
     }
 
     @Override
@@ -76,16 +151,23 @@ public class DownloadButon extends View {
         int paddingTop = getPaddingTop();
         int paddingRight = getPaddingRight();
         int paddingBottom = getPaddingBottom();
-
-        int contentWidth = getWidth() - paddingLeft - paddingRight;
-        int contentHeight = getHeight() - paddingTop - paddingBottom;
-        canvas.drawArc(new RectF(paddingLeft + paintWidth / 2, paddingTop + paintWidth / 2, getWidth() - paddingRight - paintWidth / 2, getHeight() - paddingBottom - paintWidth / 2), startAngle, currentPosition * 360 / totalPosition, false, paint);
-        int width = startDrawable.getIntrinsicWidth();
-        int height = startDrawable.getIntrinsicHeight();
+        int width = drawDrawable.getIntrinsicWidth();
+        int height = drawDrawable.getIntrinsicHeight();
         int left = (getWidth() - width) / 2;
         int top = (getHeight() - height) /2;
-        startDrawable.setBounds(left, top, left + width,top + height);
-        startDrawable.draw(canvas);
-        
+        RectF rectF = new RectF(paddingLeft + paintWidth / 2, paddingTop + paintWidth / 2, getWidth() - paddingRight - paintWidth / 2, getHeight() - paddingBottom - paintWidth / 2);
+        if(currentPosition < totalPosition){
+            canvas.drawArc(rectF, startAngle, currentPosition * 360 / totalPosition, false, paint);
+            drawDrawable.setBounds(left, top, left + width,top + height);
+            drawDrawable.draw(canvas);
+        }
+        else{
+            RectF rectF1 = new RectF(paddingLeft + paintWidth, paddingTop + paintWidth, getWidth() - paddingRight - paintWidth, getHeight() - paddingBottom - paintWidth);
+            canvas.drawArc(rectF, startAngle, 360, false, paint);
+            drawDrawable.setBounds(left, drawableTop, left + width, drawableTop + height);
+            drawDrawable.draw(canvas);
+        }
+
+
     }
 }
--- app/src/main/java/com/konka/downloadbuttontest/MainActivity.java
+++ app/src/main/java/com/konka/downloadbuttontest/MainActivity.java
@@ -2,6 +2,8 @@ package com.konka.downloadbuttontest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.konka.customui.DownloadButon;
 
@@ -11,8 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        DownloadButon buton = (DownloadButon) findViewById(R.id.button);
-        buton.setTotalPosition(360);
-        buton.setCurrentPosition(90);
+        DownloadButon button = (DownloadButon) findViewById(R.id.button);
+        button.setTotalPosition(360);
+        button.setCurrentPosition(260);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((DownloadButon) view).setCurrentPosition(360);
+                Log.i("test","click");
+            }
+        });
     }
 }
