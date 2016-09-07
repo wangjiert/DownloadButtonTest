74724c44dfa9defa9a759b75c98851eabb3f77e1
wangjiert <wangjiert@gmail.com>
2016-09-07 14:24:45 +0800

    增加画弧的功能

--- app/src/main/java/com/konka/downloadbuttontest/MainActivity.java
+++ app/src/main/java/com/konka/downloadbuttontest/MainActivity.java
@@ -3,11 +3,16 @@ package com.konka.downloadbuttontest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.konka.customui.DownloadButon;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DownloadButon buton = (DownloadButon) findViewById(R.id.button);
+        buton.setTotalPosition(360);
+        buton.setCurrentPosition(90);
     }
 }
